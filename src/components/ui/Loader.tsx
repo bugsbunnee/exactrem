ecdb20b4a473ad898201525bf928a194ab76fad4
marcel <chukwuma.marcel@quickcheck.ng>
2024-09-08 13:58:40 +0100

    implementing-loader-ui

---
+++ src/components/ui/Loader.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React from 'react';
+
+import { navigationItems } from '@/utils/constants';
+import { Container, Flex, Skeleton, Spinner } from '@radix-ui/themes';
+
+const Loader = () => {
+    return (
+        <>
+            <Flex className="border-b-2 bg-white dark:bg-[#222] sticky top-0 z-50">
+                <Container>
+                    <Flex align="center" justify="between" className="h-16">
+                        <Skeleton className='w-10 h-4' />
+
+                        <Flex flexGrow="1" gap="7" align="center" justify="center">
+                            {navigationItems.map((navigationItem) => (
+                                <Skeleton key={navigationItem.label} className='w-10 h-4' />
+                            ))}
+                        </Flex>
+
+                        <Flex gap="3" align="center">
+                            <Skeleton className='w-4 h-4' />
+                            <Skeleton className='w-4 h-4' />
+                            <Skeleton className='w-4 h-4' />
+                        </Flex>
+                    </Flex>
+                </Container>
+            </Flex>
+
+            <Flex justify='center' align='center' className='bg-stone-50 w-dvw h-dvh z-50'>
+                <Spinner />
+            </Flex>
+        </>
+    )
+};
+
+export default Loader;
