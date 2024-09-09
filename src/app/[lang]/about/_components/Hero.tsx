db7a013aebb4c81e7b3c78c3aa3748b7b879b943
marcel <chukwuma.marcel@quickcheck.ng>
2024-09-09 11:45:21 +0100

    fix

--- src/app/[lang]/about/_components/Hero.tsx
+++ src/app/[lang]/about/_components/Hero.tsx
@@ -1,10 +1,6 @@
 "use client";
 
 import React from 'react';
-
-import Conditional from '@/components/common/Conditional';
-import Link from 'next/link';
-
 import { Box, Button, Flex, Heading, Text } from '@radix-ui/themes';
 
 import useDictionary from '@/hooks/useDictionary';
@@ -17,24 +13,22 @@ const AboutHero = () => {
 			<Box className="text-center w-full flex justify-center items-center">
 			    <Box className="text-center max-w-[45rem]">
                     <Heading className="leading-10 font-bold" size="9">
-                        About <span className='text-orange-600'>Exactrem</span>
+                        {dictionary.page.about.title.one} <span className='text-orange-600'>{dictionary.page.about.title.two}</span>
                     </Heading>
 
                     <Text as="p" className="my-12 leading-7" size="3">
-                        Exactrem Canada is a MSB licenced Company has been approved 
-                        by FINTRAC for Money transferring services, Dealing in virtual 
-                        currencies and Payment service provider services.
+                        {dictionary.page.about.description}
                     </Text>
 			    </Box>
 			</Box>
 
 			<Flex justify="center" align="center" gap="4">
 				<Button  size='4' className="bg-stone-950 text-sm font-semibold">
-                    Meet the team
+                    {dictionary.page.about.cta_one}
                 </Button>
 				
                 <Button variant='outline' size='4' color='orange' className="text-sm font-semibold">
-                    Our investors
+                    {dictionary.page.about.cta_two}
                 </Button>
 			</Flex>
 		</section>
