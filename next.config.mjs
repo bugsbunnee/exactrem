4bf200feb7f14a2cd94bdcaa9b10d2301151cb31
marcel <chukwuma.marcel@quickcheck.ng>
2024-09-09 11:54:51 +0100

    fixing-velite-plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,44 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-	reactStrictMode: true,
-	images: {
-		unoptimized: true,
-		domains: [
-			'cdn.dummyjson.com',
-			'flagsapi.com',
-			'flagcdn.com',
-			'upload.wikimedia.org',
-		],
-	},
-	webpack: config => {
-	  config.plugins.push(new VeliteWebpackPlugin())
-	  return config
-	}
-};
-  
-class VeliteWebpackPlugin {
-	static started = false;
+const isDev = process.argv.indexOf('dev') !== -1;
+const isBuild = process.argv.indexOf('build') !== -1;
 
-	constructor(/** @type {import('velite').Options} */ options = {}) {
-		this.options = options;
-	}
-
-	apply(/** @type {import('webpack').Compiler} */ compiler) {
-		// executed three times in nextjs
-		// twice for the server (nodejs / edge runtime) and once for the client
-		compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
-			if (VeliteWebpackPlugin.started) return;
-			VeliteWebpackPlugin.started = true;
-			const dev = compiler.options.mode === 'development';
-			const { build } = await import('velite')
-			
-			this.options.watch = this.options.watch ?? dev;
-			this.options.clean = this.options.clean ?? !dev;
-			
-			await build(this.options);
-		})
-	}
+if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
+	process.env.VELITE_STARTED = '1';
+	
+	const { build } = await import('velite');
+	await build({ watch: isDev, clean: !isDev })
 }
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
