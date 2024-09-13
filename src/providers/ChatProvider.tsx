4d2e2145bb3d69bc309684de5dae4d3f18e7251e
marcel <chukwuma.marcel@quickcheck.ng>
2024-09-13 17:44:53 +0100

    final-bugfix

--- src/providers/ChatProvider.tsx
+++ src/providers/ChatProvider.tsx
@@ -4,13 +4,14 @@ import { Spinner } from "@radix-ui/themes";
 
 import { FC, PropsWithChildren } from "react";
 import { Chat, useCreateChatClient } from "stream-chat-react";
-import { ChatSession } from "@/hooks/useChats";
 
 import { ChatClient } from "@/utils/models";
 
 import 'stream-chat-react/dist/css/v2/index.css';
-
-type Props = PropsWithChildren & ChatSession;
+interface Props extends PropsWithChildren {
+    userId: string;
+    userToken: string;
+}
 
 const ChatProvider: FC<Props> = ({ children, userId, userToken }) => {
     const client: ChatClient | null = useCreateChatClient({
