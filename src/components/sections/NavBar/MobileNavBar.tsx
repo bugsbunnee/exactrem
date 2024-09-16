e99fdf18a900f078386ca58c7070ee3a2355dfda
marcel <chukwuma.marcel@quickcheck.ng>
2024-09-16 22:48:00 +0100

    fixing-service-images

--- src/components/sections/NavBar/MobileNavBar.tsx
+++ src/components/sections/NavBar/MobileNavBar.tsx
@@ -23,10 +23,10 @@ const MobileNavBar = () => {
         <Collapsible.Root className="relative" open={open} onOpenChange={setOpen}>
             <Collapsible.Trigger asChild>
                 <IconButton variant="ghost" size="3">
-                    {open ? <Cross1Icon width="20" height="20" className="text-white animate__animated animate__fadeInLeft" /> : <CgMenuRight size={30} className="text-white animate__animated animate__fadeInRight" />}
+                    {open ? <Cross1Icon width="20" height="20" className="text-white dark:text-black animate__animated animate__fadeInLeft" /> : <CgMenuRight size={30} className="text-white dark:text-black animate__animated animate__fadeInRight" />}
                 </IconButton>
             </Collapsible.Trigger>
-            <Collapsible.Content className='bg-white p-5 top-16 left-0 right-0 fixed z-10 min-h-screen w-screen overflow-y-scroll animate__animated animate__fadeIn'>
+            <Collapsible.Content className='bg-white dark:bg-[#222] p-5 top-16 left-0 right-0 fixed z-10 min-h-screen w-screen h-dvh overflow-y-scroll animate__animated animate__fadeIn'>
                 {navigationItems.map((item, index) => (
                     <MobileNavOption 
                         showSeparator={(index + 1) !== navigationItems.length} 
@@ -37,7 +37,7 @@ const MobileNavBar = () => {
 
                 <Separator size="4" className="my-4" />
 
-                <Flex justify='center' align='center' mt='6'>
+                <Flex justify='center' align='center'>
                     <AppButton route='/register' label={dictionary.components.navbar.sign_up} />
                 </Flex>
             </Collapsible.Content>
