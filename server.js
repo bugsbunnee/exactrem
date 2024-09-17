8de0d282c523d2ccba9431945003c232c110c750
marcel <chukwuma.marcel@quickcheck.ng>
2024-09-18 00:13:53 +0100

    fixes

---
+++ server.js
@@ -0,0 +1,47 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = 'localhost';
+const port = process.env.PORT || 3000;
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+async function initServer() {
+    await app.prepare();
+
+    createServer(async (request, response) => {
+        try {
+            const parsedUrl = parse(request.url, true);
+            const { pathname, query } = parsedUrl;
+
+            if (pathname === '/a') {
+                return await app.render(request, response, '/a', query);
+            }
+            
+            if (pathname === '/b') {
+                return await app.render(request, response, '/b', query);
+            } 
+
+            await handle(request, response, parsedUrl);
+        } catch (err) {
+            console.error('Error occurred handling', req.url, err);
+          
+            response.statusCode = 500;
+            response.end('internal server error');
+        }
+    })
+    .once('error', (error) => {
+        console.log(error);
+        process.exit(1);
+    })
+    .listen(port, (error) => {
+        if (error) throw error;
+
+        console.log(`> Ready on http://${hostname}:${port}`);
+    });
+}
+
+initServer();
